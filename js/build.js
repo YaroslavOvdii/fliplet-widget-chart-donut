47283d77feff7994944901a971fb4d41861f5fcb
Tony Wu <Tony@Tony.home>
2017-05-21 23:02:19 +0100

    Tweak

--- js/build.js
+++ js/build.js
@@ -1,5 +1,5 @@
 window.ui = window.ui || {}
-ui.flipletCharts = {};
+ui.flipletCharts = ui.flipletCharts || {};
 Fliplet().then(function(){
   $('[data-chart-donut-id]').each(function (i, el) {
     var chartId = $(this).data('chart-donut-id');
